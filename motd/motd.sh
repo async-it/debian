48e5eb22d62f0e88c74b4c1ba639ff7ee3d26672
Async IT Sàrl <70369976+async-it@users.noreply.github.com>
2025-04-08 08:44:45 +0200

    Update motd.sh

--- motd/motd.sh
+++ motd/motd.sh
@@ -4,7 +4,7 @@
 
 #V1.0: Initial Release
 
-wget -qO /etc/update-motd.d/01-async https://github.com/Tontonjo/debian/raw/master/motd/01-async
+wget -qO /etc/update-motd.d/01-async https://github.com/async-it/debian/raw/master/motd/01-async
 chmod +x /etc/update-motd.d/01-async
 cp /etc/motd /etc/motd.bck && rm -rf /etc/motd
 rm -rf /etc/update-motd.d/10-uname
