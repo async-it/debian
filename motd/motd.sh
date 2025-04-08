d7c80dfb065e9457b17d93014f4d967b9559a29d
Async IT Sàrl <70369976+async-it@users.noreply.github.com>
2025-04-08 09:55:47 +0200

    Update motd.sh

--- motd/motd.sh
+++ motd/motd.sh
@@ -3,8 +3,9 @@
 # Async IT Sàrl - Jonas Sauge
 
 #V1.0: Initial Release
+#V1.1: prevent copy to throw error if file does not exist
 
 wget -qO /etc/update-motd.d/01-async https://raw.githubusercontent.com/async-it/debian/refs/heads/main/motd/01-async
 chmod +x /etc/update-motd.d/01-async
-cp /etc/motd /etc/motd.bck && rm -rf /etc/motd
+[[ -e /etc/motd ]] && cp /etc/motd /etc/motd.bck && rm -rf /etc/motd
 rm -rf /etc/update-motd.d/10-uname
